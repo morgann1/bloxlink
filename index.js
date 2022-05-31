bc6d0dd73ea20c4fdfbbac5d5aef54773457ba57
morgann1 <58310810+morgann1@users.noreply.github.com>
2022-05-31 11:31:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const Configuration = require("./lib/Configuration.js");
+const BloxlinkAPI = require("./lib/BloxlinkAPI.js");
+
+module.exports.Configuration = Configuration;
+module.exports.BloxlinkAPI = BloxlinkAPI;
